d7138c91ecb23c96fa58b9724449c2acfaa73f3f
lzh65038-web <lzh65038@gmail.com>
2026-05-06 23:21:36 +0800

    Stack-3
    复现栈

---
+++ Stack-3/Stack-3/test.c
@@ -0,0 +1,26 @@
+﻿#include "Stack.h"
+
+int main()
+{
+	Stack st;
+	STInit(&st);
+
+	STPush(&st, 1);
+	STPush(&st, 2);
+	STPush(&st, 3);
+	STPush(&st, 4);
+	STPush(&st, 5);
+	STPush(&st, 6);
+	STPush(&st, 7);
+
+
+	while (!STEmpty(&st))
+	{
+		printf("%d ", STTop(&st));
+		STPop(&st);
+	}
+
+	STDestroy(&st);
+
+	return 0;
+}
